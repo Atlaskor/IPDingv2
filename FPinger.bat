165961d4b895ba811d0920901ce8ea099b6c5df4
Atlaskor <atlaskor@atlaskorservers.com>
2025-03-19 16:51:30 -0600

    Update FPinger.bat
    Added call for MergePlease.bat to extend the usefulness of the script.

--- FPinger.bat
+++ FPinger.bat
@@ -1,12 +1,3 @@
-@echo off
-setlocal enabledelayedexpansion
-REM Run ipex.bat if needed
-echo Calling ipex.bat...
-call ipex.bat
-echo Success!
-REM handle time formatting for files to be saved
-for /f "tokens=2 delims==" %%I in ('"wmic os get localdatetime /value"') do set datetime=%%I
-REM format the date
 set timestamp=%datetime:~0,4%%datetime:~4,2%%datetime:~6,2%_%datetime:~8,2%%datetime:~10,2%%datetime:~12,2%
 REM Define the file paths
 set "ipfile=iplist.txt"
@@ -20,11 +11,15 @@ for /f "usebackq delims=" %%i in ("%ipfile%") do (
     ping -n 1 -w 30 %%i >nul
     if errorlevel 1 (
         echo %%i is down...
+		echo %%i down... >> "C:\Users\dpritchard\desktop\runs\down_%timestamp%.txt"
     ) else (
         echo %%i is up!
-		echo %%i up >> "C:\Users\dpritchard\OneDrive - Mountain West Technology Networks\runs\up_%timestamp%.txt"
+		echo %%i up >> "C:\Users\dpritchard\desktop\runs\up_%timestamp%.txt"
     )
 )
 REM Notify user that script has completed
 echo Ping test completed. Results saved to %outfile%.
+pause
+echo Calling mergeplease.bat.....
+call MergePlease.bat
 pause
