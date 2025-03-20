9f5add55de188b6b7c9e352a66368cbbe353c2f5
Atlaskor <atlaskor@atlaskorservers.com>
2025-03-19 18:54:00 -0600

    Update FPinger.bat
    I'm an idiot.

--- FPinger.bat
+++ FPinger.bat
@@ -1,3 +1,12 @@
+@echo off
+setlocal enabledelayedexpansion
+REM Run ipex.bat if needed
+echo Calling ipex.bat...
+call ipex.bat
+echo Success!
+REM handle time formatting for files to be saved
+for /f "tokens=2 delims==" %%I in ('"wmic os get localdatetime /value"') do set datetime=%%I
+REM format the date
 set timestamp=%datetime:~0,4%%datetime:~4,2%%datetime:~6,2%_%datetime:~8,2%%datetime:~10,2%%datetime:~12,2%
 REM Define the file paths
 set "ipfile=iplist.txt"
